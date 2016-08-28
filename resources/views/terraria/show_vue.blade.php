11e105d8f0b5c828f80be944cef287e1fa31fa1f
pub@matthias-henning.de <pub@matthias-henning.de>
2016-08-28 14:24:38 +0200

    fixed missing links

--- resources/views/terraria/show_vue.blade.php
+++ resources/views/terraria/show_vue.blade.php
@@ -11,10 +11,10 @@
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="material-icons">settings</i></a>
                         <ul class="dropdown-menu" role="menu">
                             <li>
-                                <a href=""><i class="material-icons">mode_edit</i> @lang('menu.edit')</a>
+                                <a href="/terraria/@{{ terrarium.id }}/edit"><i class="material-icons">mode_edit</i> @lang('menu.edit')</a>
                             </li>
                             <li>
-                                <a href=""><i class="material-icons">delete</i> @lang('menu.delete')</a>
+                                <a href="/terraria/@{{ terrarium.id }}/delete"><i class="material-icons">delete</i> @lang('menu.delete')</a>
                             </li>
                         </ul>
                     </li>
