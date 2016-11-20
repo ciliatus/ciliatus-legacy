27b6dde0ce7bee903c58752e2f72857983dc48a6
pub@matthias-henning.de <pub@matthias-henning.de>
2016-11-20 15:28:52 +0100

    fixed sparkline graph positioning

--- resources/views/terraria/vue/show.blade.php
+++ resources/views/terraria/vue/show.blade.php
@@ -4,11 +4,11 @@
             <div class="card">
                 <div class="card-image waves-effect waves-block waves-light terrarium-card-image" v-bind:style="'background-image: url(\'' + terrarium.default_background_filepath + '\');'">
                     <div>
-                        <inline-graph :parentid="terrarium.id" graphtype="humidity_percent" type="line" :options="{'fill': null, 'strokeWidth': '3', 'stroke': '#2196f3', width: '100%', height:'60px', min: 1, max: 99}" :source="'/api/v1/terraria/'+terrarium.id+'/sensorreadingsByType/humidity_percent'"></inline-graph>
+                        <inline-graph :parentid="terrarium.id" graphtype="humidity_percent" type="line" :options="{'fill': null, 'strokeWidth': '3', 'stroke': '#2196f3', width: '100%', height:'140px', min: 1, max: 99}" :source="'/api/v1/terraria/'+terrarium.id+'/sensorreadingsByType/humidity_percent'"></inline-graph>
                     </div>
 
-                    <div style="position: relative; top: -60px">
-                        <inline-graph :parentid="terrarium.id" graphtype="temperature_celsius" type="line" :options="{'fill': null, 'strokeWidth': '3', 'stroke': '#b71c1c', width: '100%', height:'60px', min: 10, max: 40}" :source="'/api/v1/terraria/'+terrarium.id+'/sensorreadingsByType/temperature_celsius'"></inline-graph>
+                    <div style="position: relative; top: -145px">
+                        <inline-graph :parentid="terrarium.id" graphtype="temperature_celsius" type="line" :options="{'fill': null, 'strokeWidth': '3', 'stroke': '#b71c1c', width: '100%', height:'140px', min: 1, max: 99}" :source="'/api/v1/terraria/'+terrarium.id+'/sensorreadingsByType/temperature_celsius'"></inline-graph>
                     </div>
                 </div>
                 <div class="card-content">
