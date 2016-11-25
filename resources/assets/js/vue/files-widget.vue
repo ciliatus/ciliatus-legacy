227dd40eae87d8035c9493b6ffe1c8f8eff2a42e
pub@matthias-henning.de <pub@matthias-henning.de>
2016-11-25 15:29:11 +0100

    fixed variables

--- resources/assets/js/vue/files-widget.vue
+++ resources/assets/js/vue/files-widget.vue
@@ -1,25 +1,25 @@
 <template>
     <div class="card">
         <div class="card-content light-blue darken-1 white-text">
-            @{{ files.length }} @choice('components.files', 2)
+            {{ files.length }} {{ $tc("components.files", 2) }}
         </div>
 
         <div class="card-content">
             <span class="card-title activator truncate">
-                <span>@choice('components.files', 2)</span>
+                <span>{{ $tc("components.files", 2) }}</span>
                 <i class="material-icons right">more_vert</i>
             </span>
             <p>
-            <div class="chip" v-for="file in files">
-                <i class="material-icons">insert_drive_file</i>
-                <a v-bind:href="'/files/' + file.id">@{{ file.display_name }}</a> <i>@{{ file.size }}</i>
-            </div>
+                <div class="chip" v-for="file in files">
+                    <i class="material-icons">insert_drive_file</i>
+                    <a v-bind:href="'/files/' + file.id">{{ file.display_name }}</a> <i>{{ file.size }}</i>
+                </div>
             </p>
         </div>
 
         <div class="card-action">
-            <a v-bind:href="'/files/create?preset[belongsTo_type]=' + belongsTo_type + '&preset[belongsTo_id]=' + belongsTo_id">@lang('buttons.add')</a>
-            <a v-bind:href="'/files/?filter[belongsTo_type]=' + belongsTo_type + '&filter[belongsTo_id]=' + belongsTo_id">@lang('buttons.details')</a>
+            <a v-bind:href="'/files/create?preset[belongsTo_type]=' + belongsTo_type + '&preset[belongsTo_id]=' + belongsTo_id">{{ $t("buttons.add") }}</a>
+            <a v-bind:href="'/files/?filter[belongsTo_type]=' + belongsTo_type + '&filter[belongsTo_id]=' + belongsTo_id">{{ $t("buttons.details") }}</a>
         </div>
 
         <div class="card-reveal">
