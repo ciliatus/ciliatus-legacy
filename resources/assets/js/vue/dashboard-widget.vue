9c599de8731401fbd959d1ebf5fa11441310923f
pub@matthias-henning.de <pub@matthias-henning.de>
2016-12-01 15:48:47 +0100

    fixed template layout, moved more critical to the top and added translations

--- resources/assets/js/vue/dashboard-widget.vue
+++ resources/assets/js/vue/dashboard-widget.vue
@@ -3,32 +3,26 @@
 
         <div class="col s12 m6 l6">
             <!-- start feedings -->
-            <div class="card">
-                <div class="card-content teal lighten-1 white-text">
-                    {{ $tc("components.animal_feedings", 2) }} {{ $t("labels.due") }}
+            <div class="card" v-show="dashboard.terraria.critical.length > 0">
+                <div class="card-content red darken-3 white-text">
+                    {{ $tc("components.terraria", 2) }}
                 </div>
 
-                <div class="card-content teal lighten-2 white-text">
+                <div class="card-content red darken-2 white-text">
                     <span class="card-title activator truncate">
-                        <span>{{ dashboard.animal_feeding_schedules.due.length }} {{ $tc("components.animal_feedings", dashboard.animal_feeding_schedules.due.length) }} {{ $t("labels.due") }}</span>
+                        <span>{{ dashboard.terraria.critical.length }} {{ $tc("components.terraria", dashboard.terraria.critical.length) }} {{ $t("labels.critical") }}</span>
                         <i class="material-icons right">more_vert</i>
                     </span>
 
-                    <div v-for="schedule in dashboard.animal_feeding_schedules.due">
+                    <div v-for="terrarium in dashboard.terraria.critical">
                         <p>
-                            <a v-bind:href="'/animals/' + schedule.animal.id" class="white-text">
-                                {{ schedule.animal.display_name }}:
-                            </a>
-
-                            <a v-bind:href="'/animals/' + schedule.animal.id + '/feeding_schedules/' + schedule.id" class="white-text">
-                                {{ $tc("labels." + schedule.type) }}
-                            </a>
+                            <a v-bind:href="'/terraria/' + terrarium.id" class="white-text">{{ terrarium.display_name }}</a>
                         </p>
                     </div>
                 </div>
 
-                <div class="card-reveal teal lighten-2 white-text">
-                    <span class="card-title teal lighten-2 white-text"><i class="material-icons right">close</i></span>
+                <div class="card-reveal red darken-2 white-text">
+                    <span class="card-title red darken-2 white-text"><i class="material-icons right">close</i></span>
 
                     <p>
                     </p>
@@ -61,6 +55,8 @@
                             <a v-bind:href="'/animals/' + schedule.animal.id + '/feeding_schedules/' + schedule.id" class="white-text">
                                 {{ $tc("labels." + schedule.type) }}
                             </a>
+
+                            ({{ $t("labels.since") }} {{ (schedule.due_days*-1) }} {{ $tc("units.days", (schedule.due_days*-1)) }})
                         </p>
                     </div>
                 </div>
@@ -85,18 +81,30 @@
             <!-- start terraria -->
             <div class="card">
                 <div class="card-content teal white-text">
-                    {{ $tc("components.terraria", 2) }}
+                    {{ $tc("components.animal_feedings", 2) }} {{ $t("labels.due") }}
                 </div>
 
                 <div class="card-content teal lighten-1 white-text">
                     <span class="card-title activator truncate">
-                        <span>{{ dashboard.terraria.ok.length }} {{ $tc("components.terraria", dashboard.terraria.ok.length) }} {{ $t("labels.ok") }}</span>
+                        <span>{{ dashboard.animal_feeding_schedules.due.length }} {{ $tc("components.animal_feedings", dashboard.animal_feeding_schedules.due.length) }} {{ $t("labels.due") }}</span>
                         <i class="material-icons right">more_vert</i>
                     </span>
+
+                    <div v-for="schedule in dashboard.animal_feeding_schedules.due">
+                        <p>
+                            <a v-bind:href="'/animals/' + schedule.animal.id" class="white-text">
+                                {{ schedule.animal.display_name }}:
+                            </a>
+
+                            <a v-bind:href="'/animals/' + schedule.animal.id + '/feeding_schedules/' + schedule.id" class="white-text">
+                                {{ $tc("labels." + schedule.type) }}
+                            </a>
+                        </p>
+                    </div>
                 </div>
 
                 <div class="card-reveal teal lighten-1 white-text">
-                    <span class="card-title teal lighten-1 white-text"><i class="material-icons right">close</i></span>
+                    <span class="card-title teal lighten-2 white-text"><i class="material-icons right">close</i></span>
 
                     <p>
                     </p>
@@ -109,26 +117,20 @@
                 -->
             </div>
 
-            <div class="card" v-show="dashboard.terraria.critical.length > 0">
-                <div class="card-content red darken-3 white-text">
+            <div class="card">
+                <div class="card-content teal white-text">
                     {{ $tc("components.terraria", 2) }}
                 </div>
 
-                <div class="card-content red darken-2 white-text">
+                <div class="card-content teal lighten-1 white-text">
                     <span class="card-title activator truncate">
-                        <span>{{ dashboard.terraria.critical.length }} {{ $tc("components.terraria", dashboard.terraria.critical.length) }} {{ $t("labels.critical") }}</span>
+                        <span>{{ dashboard.terraria.ok.length }} {{ $tc("components.terraria", dashboard.terraria.ok.length) }} {{ $t("labels.ok") }}</span>
                         <i class="material-icons right">more_vert</i>
                     </span>
-
-                    <div v-for="terrarium in dashboard.terraria.critical">
-                        <p>
-                            <a v-bind:href="'/terraria/' + terrarium.id" class="white-text">{{ terrarium.display_name }}</a>
-                        </p>
-                    </div>
                 </div>
 
-                <div class="card-reveal red darken-2 white-text">
-                    <span class="card-title red darken-2 white-text"><i class="material-icons right">close</i></span>
+                <div class="card-reveal teal lighten-1 white-text">
+                    <span class="card-title teal lighten-1 white-text"><i class="material-icons right">close</i></span>
 
                     <p>
                     </p>
