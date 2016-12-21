642ca0c7565d5b8b91b9f0a0a9cb7743982a979d
pub@matthias-henning.de <pub@matthias-henning.de>
2016-12-21 15:13:03 +0100

    view is now tabbed

--- resources/views/animals/index.blade.php
+++ resources/views/animals/index.blade.php
@@ -6,8 +6,24 @@
 
 
 @section('content')
-    <div class="container">
-        <animals-widget container-classes="row" wrapper-classes="col s12 m6 l4"></animals-widget>
+    <div class="col s12">
+        <ul class="tabs z-depth-1">
+            <li class="tab col s3"><a class="active" href="#tab_dashboard">@lang('labels.overview')</a></li>
+            <li class="tab col s3"><a href="#tab_feeding_schedules">@choice('components.animal_feeding_schedules', 2)</a></li>
+        </ul>
+    </div>
+
+    <div id="tab_dashboard" class="col s12">
+        <div class="container">
+            <animals-widget container-classes="row" wrapper-classes="col s12 m6 l4"></animals-widget>
+        </div>
+    </div>
+
+    <div id="tab_feeding_schedules" class="col s12">
+        <div class="container">
+            <p>@lang('tooltips.animal_feeding_schedule_matrix')</p>
+            <animal_feeding_schedules-matrix-widget container-classes="row" wrapper-classes=""></animal_feeding_schedules-matrix-widget>
+        </div>
     </div>
 
     <div class="fixed-action-btn">
