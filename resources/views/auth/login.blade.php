c0d146609e060d57f1a5e7f8c9b8589427f83037
pub@matthias-henning.de <pub@matthias-henning.de>
2016-12-22 11:37:50 +0100

    updated login form to card design

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,36 +14,27 @@
     </head>
 
     <body>
-        <div class="container">
+        <div class="container" style="padding-top: 100px">
             <div class="row">
                 <div class="col s12 m6 offset-m3 l4 offset-l4">
-                    <form method="POST" action="{{ url('auth/login') }}">
-                        {!! csrf_field() !!}
-                        <h1>Login Form</h1>
-                        <div>
-                            <input type="text" class="form-control" placeholder="Username" required="" name="email" value="{{ old('email') }}" />
-                        </div>
-                        <div>
-                            <input type="password" class="form-control" placeholder="Password" required="" name="password" id="password" />
-                        </div>
-                        <div>
-                            <input type="checkbox" name="remember"> Remember Me
-                        </div>
-                        <div>
-                            <button class="btn btn-default submit" type="submit">Log in</button>
-                        </div>
+                    <div class="card">
+                        <form method="POST" action="{{ url('auth/login') }}">
+                            {!! csrf_field() !!}
+                            <div class="card-content">
+                                <span class="card-title">Ciliatus</span>
 
-                        <div class="clearfix"></div>
-
-                        <div class="separator">
-                            <p class="change_link">New to site?
-                                <a href="{{ url('auth/register') }}" class="to_register"> Create Account </a>
-                            </p>
-
-                            <div class="clearfix"></div>
-                            <br />
-                        </div>
-                    </form>
+                                <div>
+                                    <input type="text" class="form-control" placeholder="Username" required="" name="email" value="{{ old('email') }}" />
+                                </div>
+                                <div>
+                                    <input type="password" class="form-control" placeholder="Password" required="" name="password" id="password" />
+                                </div>
+                                <div>
+                                    <button class="btn btn-default submit" type="submit">Log in</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
