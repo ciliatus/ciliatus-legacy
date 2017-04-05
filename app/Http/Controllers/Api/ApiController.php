7d36ef54ae4fc27bd98f4af3eb662d996cd6eab3
pub@matthias-henning.de <pub@matthias-henning.de>
2017-04-05 19:47:07 +0200

    moved API filtering to own package

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -7,6 +7,7 @@ use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Http\Request;
+use Matthenning\EloquentApiFilter\EloquentApiFilter;
 
 /**
  * Class ApiController
@@ -234,23 +235,6 @@ class ApiController extends Controller
         return null;
     }
 
-    /*
-     * .../?filter[field]=operator:comparison
-     * .../?filter[field]=operator
-     *
-     * Example query:
-     * .../?filter[name]=like:*s00hvm*&filter[status]=null:
-     * will match all entities where name contains s00hvm and status is null
-     *
-     * Multiple filters on one field can be chained:
-     * .../?filter[created_at]=lt:2016-12-10:and:gt:2016-12-08
-     *
-     * Filter by related models' fields by using the dot-notaion:
-     * .../?filter[physical_sensor.logical_sensor.name]=Sensor1
-     *
-     * special operators:
-     * like, notlike, today, nottoday, null, notnull
-     */
     /**
      * @param Request $request
      * @param $query
@@ -258,200 +242,8 @@ class ApiController extends Controller
      */
     protected function filter(Request $request, Builder $query)
     {
-        if ($request->has('filter')) {
-            foreach ($request->input('filter') as $field=>$value) {
-                $query = $this->applyFieldFilter($query, $field, $value);
-            }
-        }
-
-        if ($request->has('order')) {
-            foreach ($request->input('order') as $field=>$value) {
-                $query = $this->applyOrder($query, $field, $value);
-            }
-        }
-
-        if ($request->has('limit')) {
-            $query = $query->limit($request->input('limit'));
-        }
-
-        return $query;
+        return (new EloquentApiFilter($request, $query))->filter();
     }
-
-    /**
-     * Resolves :and: links and applies each filter
-     *
-     * @param Builder $query
-     * @param $field
-     * @param $value
-     * @return Builder
-     */
-    private function applyFieldFilter(Builder $query, $field, $value)
-    {
-        $filters = explode(':and:', $value);
-        foreach ($filters as $filter) {
-            $query = $this->applyFilter($query, $field, $filter);
-        }
-
-        return $query;
-    }
-
-    /**
-     * Applies a single filter
-     *
-     * @param Builder $query
-     * @param $field
-     * @param $filter
-     * @return Builder
-     */
-    private function applyFilter(Builder $query, $field, $filter)
-    {
-        $filter = explode(':', $filter);
-        if (count($filter) > 1) {
-            $operator = $this->getFilterOperator($filter[0]);
-            $value = $this->replaceWildcards($filter[1]);
-        }
-        else {
-            $operator = '=';
-            $value = $this->replaceWildcards($filter[0]);
-        }
-
-        $fields = explode('.', $field);
-        if (count($fields) > 1) {
-            return $this->applyNestedFilter($query, $fields, $operator, $value);
-        }
-        else {
-            return $this->applyWhereClause($query, $field, $operator, $value);
-        }
-    }
-
-    /**
-     * Applies a nested filter.
-     * Meaning a filter on a related field
-     *
-     * @param Builder $query
-     * @param array $fields
-     * @param $operator
-     * @param $value
-     * @return Builder
-     */
-    private function applyNestedFilter(Builder $query, array $fields, $operator, $value)
-    {
-        $relation_name = implode('.', array_slice($fields, 0, count($fields) - 1));
-        $relation_field = $fields[count($fields) - 1];
-
-        $that = $this;
-
-        return $query->whereHas($relation_name, function ($query) use ($relation_field, $operator, $value, $that) {
-            $query = $that->applyWhereClause($query, $relation_field, $operator, $value);
-        });
-    }
-
-    /**
-     * Applies a where clause.
-     * Is used by applyFilter and applyNestedFilter
-     * to apply the clause to the query.
-     *
-     * @param Builder $query
-     * @param $field
-     * @param $operator
-     * @param $value
-     * @return Builder
-     */
-    private function applyWhereClause(Builder $query, $field, $operator, $value) {
-        switch ($value) {
-            case 'today':
-                return $query->where($field, 'like', Carbon::now()->format('Y-m-d') . '%');
-            case 'nottoday':
-                return $query->where(function ($q) use ($field) {
-                    $q->where($field, 'not like', Carbon::now()->format('Y-m-d') . '%')
-                        ->orWhereNull($field);
-                });
-            case 'null':
-                return $query->whereNull($field);
-            case 'notnull':
-                return $query->whereNotNull($field);
-            default:
-                return $query->where($field, $operator, $value);
-        }
-    }
-
-    /**
-     * @param Builder $query
-     * @param $field
-     * @param $value
-     * @return mixed
-     */
-    private function applyOrder(Builder $query, $field, $value)
-    {
-        $fields = explode('.', $field);
-        if (count($fields) > 1) {
-            return $this->applyNestedOrder($fields[0], $query, $fields[1], $value);
-        }
-        else {
-            return $this->applyOrderByClause($query, $field, $value);
-        }
-    }
-
-    /**
-     * @TODO: This does not work yet. Order by doesn't seem to support this the same way whereHas does
-     *
-     * @param $relation_name
-     * @param Builder $query
-     * @param $relation_field
-     * @param $value
-     * @return mixed
-     */
-    private function applyNestedOrder($relation_name, Builder $query, $relation_field, $value)
-    {
-        $that = $this;
-        return $query->orderBy($relation_name, function ($query) use ($relation_field, $value, $that) {
-            $query = $that->applyOrderByClause($query, $relation_field, $value);
-        });
-    }
-
-    /**
-     * @param Builder $query
-     * @param $field
-     * @param $value
-     * @return mixed
-     */
-    private function applyOrderByClause(Builder $query, $field, $value)
-    {
-        return $query->orderBy($field, $value);
-    }
-
-    /**
-     * Replaces * wildcards with %
-     * for usage in SQL
-     *
-     * @param $value
-     * @return mixed
-     */
-    private function replaceWildcards($value)
-    {
-        return str_replace('*', '%', $value);
-    }
-
-    /**
-     * Replaces operators from request string
-     * for usage in SQL
-     *
-     * @param $filter
-     * @return mixed
-     */
-    private function getFilterOperator($filter)
-    {
-        $operator = str_replace('notlike', 'not like', $filter);
-        $operator = str_replace('gt', '>', $operator);
-        $operator = str_replace('ge', '>=', $operator);
-        $operator = str_replace('lt', '<', $operator);
-        $operator = str_replace('le', '<=', $operator);
-        $operator = str_replace('eq', '=', $operator);
-
-        return $operator;
-    }
-
-
 
     /**
      * Converts selected fields from data array
