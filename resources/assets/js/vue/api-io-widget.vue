6a7c948aa563edef702055c97d4a8c857cea763e
pub@matthias-henning.de <pub@matthias-henning.de>
2017-04-29 18:29:28 +0200

    added voice recognition and text to speech for answers

--- resources/assets/js/vue/api-io-widget.vue
+++ resources/assets/js/vue/api-io-widget.vue
@@ -4,17 +4,17 @@
             <div class="input-field inline">
                 <form action="/api/v1/apiai/send_request" data-method="POST" id="api-io-widget-form"
                       :data-no-confirm="true" v-on:submit="submit_interceptor">
-                    <input name="speech" id="ask_me_something" type="text"
-                           :readonly="loading"
+                    <input name="speech" id="ask-me-something" type="text"
+                           :readonly="loading" v-model="transcript"
                            class="validate" style="padding: 0;" autocomplete="off">
-                    <label for="ask_me_something" class="white-text">{{ $t('labels.ask_me_something') }}</label>
+                    <label for="ask-me-something" id="ask-me-something-label" class="white-text">{{ $t('labels.ask_me_something') }}</label>
                 </form>
             </div>
 
 
-            <a class="btn-floating waves-effect waves-light" v-show="!loading">
-                <i v-show="!recording" v-on:click="record" class="material-icons">mic_none</i>
-                <i v-show="recording" v-on:click="record" class="material-icons">mic</i>
+            <a class="btn-floating waves-effect waves-light" v-show="!loading" v-on:mousedown="record" v-on:mouseup="stop_recording">
+                <i v-show="!recording" class="material-icons">mic_none</i>
+                <i v-show="recording" class="material-icons">mic</i>
             </a>
             <div class="preloader-wrapper small active" v-show="loading"
                  style="margin-left: 18px; margin-right: 18px; top: 10px;">
@@ -40,7 +40,10 @@ export default {
         return {
             loading: false,
             recording: false,
-            result: null
+            recording_capability: true,
+            result: null,
+            recognition: null,
+            transcript: ''
         }
     },
 
@@ -64,23 +67,72 @@ export default {
 
             this.result = data.data;
             this.loading = false;
+            var result_text = ''
 
             if (this.result.data.api_result.result !== undefined) {
-                $('#api-io-widget-result-modal-content').html('' + this.result.data.api_result.result.fulfillment.speech);
+                result_text = this.result.data.api_result.result.fulfillment.speech;
             }
             else {
-                $('#api-io-widget-result-modal-content').html('' + this.result.data.api_result);
+                result_text = this.result.data.api_result
             }
+
+            $('#api-io-widget-result-modal-content').html(result_text);
             $('#api-io-widget-result-modal').modal('open');
+            var msg = new SpeechSynthesisUtterance(result_text);
+            window.speechSynthesis.speak(msg);
+            //$('#ask-me-something').val('');
 
         },
-        record: function () {
+        record: function() {
             this.recording = !this.recording;
+            if (this.recording) {
+                this.recognition.start();
+            }
+            else {
+                this.stop();
+            }
+        },
+        stop_recording: function(){
+            this.recording = false;
+            this.recognition.stop();
         }
     },
 
     created: function() {
         window.eventHubVue.$on('FormSubmitReturnedSuccess', this.parse_result);
+        if (!('webkitSpeechRecognition' in window)) {
+            this.recording_capability = false;
+        }
+        else {
+            var that = this;
+            this.recognition = new webkitSpeechRecognition();
+            this.recognition.continuous = true;
+            this.recognition.interimResults = true;
+            this.recognition.lang = 'de';
+
+            this.recognition.onstart = function() {
+                that.recording = true;
+                $('#ask-me-something-label').addClass('active');
+            };
+            this.recognition.onresult = function(e) {
+                var transcript = '';
+
+                for (var i = e.resultIndex; i < e.results.length; ++i) {
+                    transcript += e.results[i][0].transcript;
+                }
+
+                that.transcript = transcript.replace('/\S/', function(m) { return m.toUpperCase(); });
+            };
+            this.recognition.onerror = function (e) {
+                that.recording = false;
+
+            };
+            this.recognition.onend = function () {
+                $('#api-io-widget-form').submit();
+                that.recording = false;
+                that.loading = true;
+            };
+        }
         this.$nextTick(function() {
             $('#api-io-widget-result-modal').modal();
         })
