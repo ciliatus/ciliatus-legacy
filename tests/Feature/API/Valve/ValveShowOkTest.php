d23933a4bcd65aa19e99f6464967fee3ac8577d6
pub@matthias-henning.de <pub@matthias-henning.de>
2017-05-30 17:36:11 +0200

    More tests

---
+++ tests/Feature/API/Valve/ValveShowOkTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\Feature\Valve;
+
+use App\Valve;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+use Tests\TestHelperTrait;
+
+/**
+ * Class ValveShowOkTest
+ * @package Tests\Feature
+ */
+class ValveShowOkTest extends TestCase
+{
+
+    use TestHelperTrait;
+
+    public function test()
+    {
+
+        $token = $this->createUserReadOnly();
+
+        $valve = Valve::create([
+            'name' => 'TestValve01'
+        ]);
+
+        $response = $this->get('/api/v1/valves/' . $valve->id, [
+            'HTTP_Authorization' => 'Bearer ' . $token
+        ]);
+        $response->assertStatus(200);
+        $response->assertJson([
+            'data' => [
+                'id' => $valve->id,
+                'name' => $valve->name
+            ]
+        ]);
+
+        $valve->delete();
+
+        $this->cleanupUsers();
+
+    }
+
+}
---
+++ tests/Feature/API/Valve/ValveStoreOkTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests\Feature\Valve;
+
+use App\Valve;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+use Tests\TestHelperTrait;
+
+/**
+ * Class ValveStoreOkTest
+ * @package Tests\Feature
+ */
+class ValveStoreOkTest extends TestCase
+{
+
+    use TestHelperTrait;
+
+    public function test()
+    {
+
+        $token = $this->createUserFullPermissions();
+
+        $response = $this->post('/api/v1/valves', [
+            'name' => 'TestValve01'
+        ], [
+            'HTTP_Authorization' => 'Bearer ' . $token
+        ]);
+        $response->assertStatus(200);
+        $response->assertJsonStructure([
+            'data' => [
+                'id'
+            ]
+        ]);
+
+        $id = $response->decodeResponseJson()['data']['id'];
+
+        $this->assertNotNull(Valve::find($id));
+
+        $this->cleanupUsers();
+
+    }
+
+}
