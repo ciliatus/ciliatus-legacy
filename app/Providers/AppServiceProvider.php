ba59a10cb542e9bda701bf847d79ae89f0130c16
pub@matthias-henning.de <pub@matthias-henning.de>
2017-05-31 23:49:38 +0200

    fixed SQL 1071

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 use App\Sensorreading;
 
@@ -14,6 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
         Sensorreading::observe(SensorreadingObserver::class);
     }
 
