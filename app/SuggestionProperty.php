313b3d5a2c83d42f8e0b5c78d58080273ed17330
pub@matthias-henning.de <pub@matthias-henning.de>
2017-08-21 19:49:20 +0200

    updated ide helper generated meta data

--- app/SuggestionProperty.php
+++ app/SuggestionProperty.php
@@ -6,7 +6,26 @@ use Illuminate\Database\Eloquent\Builder;
 
 /**
  * Class SuggestionProperty
+ *
  * @package App
+ * @property string $id
+ * @property string $belongsTo_type
+ * @property string $belongsTo_id
+ * @property string $type
+ * @property string $name
+ * @property bool $value
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Property[] $properties
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\SuggestionProperty whereBelongsToId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\SuggestionProperty whereBelongsToType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\SuggestionProperty whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\SuggestionProperty whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\SuggestionProperty whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\SuggestionProperty whereType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\SuggestionProperty whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\SuggestionProperty whereValue($value)
+ * @mixin \Eloquent
  */
 class SuggestionProperty extends Property
 {
