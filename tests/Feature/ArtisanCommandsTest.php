b17ab0fadb52db2b169e47aee9a3166cb889281b
pub@matthias-henning.de <pub@matthias-henning.de>
2018-01-07 14:03:59 +0100

    test now creates some models as payload

--- tests/Feature/ArtisanCommandsTest.php
+++ tests/Feature/ArtisanCommandsTest.php
@@ -2,10 +2,15 @@
 
 namespace Tests\Feature\API\Animal;
 
+use App\LogicalSensor;
+use App\PhysicalSensor;
+use App\Sensorreading;
+use App\Terrarium;
 use Artisan;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Ramsey\Uuid\Uuid;
 use Tests\TestCase;
 use Tests\TestHelperTrait;
 
@@ -21,6 +26,25 @@ class ArtisanCommandsTest extends TestCase
     public function test()
     {
 
+        $terrarium = Terrarium::create([
+            'name' => 'Terrarium01'
+        ]);
+        $ps = PhysicalSensor::create([
+            'name' => 'PS01',
+            'belongsTo_type' => 'Terrarium',
+            'belongsTo_id' => $terrarium->id
+        ]);
+        $ls = LogicalSensor::create([
+            'name' => 'LS01',
+            'physical_sensor_id' => $ps->id,
+            'type' => 'humidity_percent'
+        ]);
+        $sr = Sensorreading::create([
+            'sensorreadinggroup_id' => Uuid::uuid4(),
+            'logical_sensor_id' => $ls->id,
+            'rawvalue' => 43
+        ]);
+
         $this->assertEquals(0, Artisan::call('ciliatus:cache:rebuild'));
         $this->assertEquals(0, Artisan::call('ciliatus:caresheets:generate'));
         $this->assertEquals(0, Artisan::call('ciliatus:convert_lang_to_json'));
@@ -28,6 +52,11 @@ class ArtisanCommandsTest extends TestCase
         $this->assertEquals(0, Artisan::call('ciliatus:detect_sensorreading_anomalies'));
         $this->assertEquals(0, Artisan::call('ciliatus:notifications:send'));
         $this->assertEquals(0, Artisan::call('ciliatus:suggestions:generate'));
+
+        $sr->delete();
+        $ls->delete();
+        $ps->delete();
+        $terrarium->delete();
 
     }
 
