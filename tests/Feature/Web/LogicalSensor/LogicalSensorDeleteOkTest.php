8fd637d6a403a0d1334cf36ecc85f22b8cf07102
pub@matthias-henning.de <pub@matthias-henning.de>
2018-01-07 11:28:02 +0100

    logical sensor web routes tests (#66)

---
+++ tests/Feature/Web/LogicalSensor/LogicalSensorDeleteOkTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Feature\Web\LogicalSensor;
+
+use App\LogicalSensor;
+use App\PhysicalSensor;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+use Tests\TestHelperTrait;
+
+/**
+ * Class LogicalSensorIndexOkTest
+ * @package Tests\Feature
+ */
+class LogicalSensorDeleteOkTest extends TestCase
+{
+
+    use TestHelperTrait;
+
+    public function test()
+    {
+
+        $user = $this->createUserWeb();
+
+        $ps = PhysicalSensor::create(['display_name' => 'LogicalSensor01']);
+        $obj = LogicalSensor::create(['display_name' => 'LogicalSensor01', 'physical_sensor_id' => $ps->id]);
+        $this->actingAs($user)->get('/physical_sensors/' . $obj->id . '/delete')->assertStatus(200);
+        $obj->delete();
+
+        $this->cleanupUsers();
+
+    }
+
+}
