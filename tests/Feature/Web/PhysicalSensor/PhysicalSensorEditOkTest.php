4270310e2309dabffab45b4f3d365119e7412b6c
pub@matthias-henning.de <pub@matthias-henning.de>
2018-01-07 11:23:11 +0100

    physical sensor web routes tests (#66)

---
+++ tests/Feature/Web/PhysicalSensor/PhysicalSensorEditOkTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Feature\Web\PhysicalSensor;
+
+use App\PhysicalSensor;
+use App\PhysicalSensorSequence;
+use App\Valve;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransphysical_sensors;
+use Tests\TestCase;
+use Tests\TestHelperTrait;
+
+/**
+ * Class PhysicalSensorIndexOkTest
+ * @package Tests\Feature
+ */
+class PhysicalSensorEditOkTest extends TestCase
+{
+
+    use TestHelperTrait;
+
+    public function test()
+    {
+
+        $user = $this->createUserWeb();
+
+        $obj = PhysicalSensor::create(['display_name' => 'PhysicalSensor01']);
+        $this->actingAs($user)->get('/physical_sensors/' . $obj->id . '/edit')->assertStatus(200);
+        $obj->delete();
+
+        $this->cleanupUsers();
+
+    }
+
+}
