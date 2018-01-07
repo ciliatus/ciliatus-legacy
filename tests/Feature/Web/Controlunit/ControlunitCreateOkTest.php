f7ad7df3b81ff52c8af347dc9cb00be6877d1b68
pub@matthias-henning.de <pub@matthias-henning.de>
2018-01-07 11:21:46 +0100

    controlunit web routes tests (#66)

---
+++ tests/Feature/Web/Controlunit/ControlunitCreateOkTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature\Web\Controlunit;
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTranscontrolunits;
+use Tests\TestCase;
+use Tests\TestHelperTrait;
+
+/**
+ * Class ControlunitIndexOkTest
+ * @package Tests\Feature
+ */
+class ControlunitCreateOkTest extends TestCase
+{
+
+    use TestHelperTrait;
+
+    public function test()
+    {
+
+        $user = $this->createUserWeb();
+
+        $this->actingAs($user)->get('/controlunits/create')->assertStatus(200);
+
+        $this->cleanupUsers();
+
+    }
+
+}
