65035568ae7816db3ef09f577c6ad1c79925d798
pub@matthias-henning.de <pub@matthias-henning.de>
2018-02-03 18:38:13 +0100

    #66 Terrarium infrastructure

---
+++ tests/Feature/API/Terrarium/TerrariumInfrastructureOkTest.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Tests\Feature\API\Terrarium;
+
+use App\GenericComponent;
+use App\GenericComponentType;
+use App\PhysicalSensor;
+use App\Terrarium;
+use App\Valve;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use phpDocumentor\Reflection\DocBlock\Tags\Generic;
+use Tests\TestCase;
+use Tests\TestHelperTrait;
+
+/**
+ * class TerrariumInfrastructureOkTest
+ * @package Tests\Feature
+ */
+class TerrariumInfrastructureOkTest extends TestCase
+{
+
+    use TestHelperTrait;
+
+    public function test()
+    {
+
+        $token = $this->createUserReadOnly();
+
+        $terrarium = Terrarium::create([
+            'name' => 'TestTerrarium01', 'display_name' => 'TestTerrarium01'
+        ]);
+
+        $valve = Valve::create([
+            'name' => 'V',
+            'terrarium_id' => $terrarium->id
+        ]);
+        $ps = PhysicalSensor::create([
+            'name' => 'V',
+            'belongsTo_type' => 'Terrarium',
+            'belongsTo_id' => $terrarium->id
+        ]);
+        $gt = GenericComponentType::create([
+            'name_singular' => 'T',
+            'name_plural' => 'T',
+            'icon' => 'T'
+        ]);
+        $generic = GenericComponent::create([
+            'belongsTo_type' => 'Terrarium',
+            'belongsTo_id' => $terrarium->id,
+            'generic_component_type_id' => $gt->id,
+            'name' => 'Comp'
+        ]);
+
+        $response = $this->get('/api/v1/terraria/' . $terrarium->id . '/infrastructure', [
+            'Authorization' => 'Bearer ' . $token
+        ]);
+        $response->assertStatus(200);
+        $response->assertJson([
+            'data' => []
+        ]);
+
+        $ps->delete();
+        $valve->delete();
+        $generic->delete();
+        $gt->delete();
+        $terrarium->delete();
+
+        $this->cleanupUsers();
+
+    }
+
+}
