e9c49df527f916a5bd71d51d50d718c3895cfbc2
pub@matthias-henning.de <pub@matthias-henning.de>
2018-02-22 14:45:21 +0100

    fixed rendering before animal data is present

--- resources/assets/js/vue/animals-widget.vue
+++ resources/assets/js/vue/animals-widget.vue
@@ -13,90 +13,92 @@
 
         <div :class="containerClasses" :id="containerId">
             <div :class="wrapperClasses" v-for="animal in animals">
-                <!-- Modals -->
-                <animal-add-feeding-modal :animalId="animal.data.id" :feedingTypes="feeding_types"
-                                         :containerId="'modal_add_weight_' + animal.data.id"> </animal-add-feeding-modal>
+                <template v-if="animal.data">
+                    <!-- Modals -->
+                    <animal-add-feeding-modal :animalId="animal.data.id" :feedingTypes="feeding_types"
+                                             :containerId="'modal_add_weight_' + animal.data.id"> </animal-add-feeding-modal>
 
-                <animal-add-weight-modal :animalId="animal.data.id"
-                                         :containerId="'modal_add_weight_' + animal.data.id"> </animal-add-weight-modal>
+                    <animal-add-weight-modal :animalId="animal.data.id"
+                                             :containerId="'modal_add_weight_' + animal.data.id"> </animal-add-weight-modal>
 
-                <!-- Card -->
-                <div class="card">
-                    <div class="card-image terrarium-card-image"
-                         v-bind:style="animal.data.default_background_filepath ? 'background-image: url(\'' + animal.data.default_background_filepath + '\');' : 'background-image: url(\'/svg/Ciliatus_Logo.svg\'); background-position: top center;'">
+                    <!-- Card -->
+                    <div class="card">
+                        <div class="card-image terrarium-card-image"
+                             v-bind:style="animal.data.default_background_filepath ? 'background-image: url(\'' + animal.data.default_background_filepath + '\');' : 'background-image: url(\'/svg/Ciliatus_Logo.svg\'); background-position: top center;'">
 
-                        <div class="card-title">
-                            <span><a :href="'/animals/' + animal.data.id">{{ animal.data.display_name }}</a></span>
-                            <loading-indicator :size="20" v-show="animal.data.loading_data"> </loading-indicator>
-                            <a href="#!"><i class="material-icons right activator" v-if="!animal.data.death_date">more_vert</i></a>
-                        </div>
-                    </div>
-
-                    <div class="card-content">
-                        <div>
-                            <span v-show="animal.data.latin_name">{{ animal.data.latin_name }},</span>
-                            <span v-show="animal.data.common_name && !animal.data.latin_name">{{ animal.data.common_name }},</span>
-                            <span v-show="animal.data.birth_date || animal.data.death_date">{{ animal.data.age_value }} {{ $tc("units." + animal.data.age_unit, animal.data.age_value) }}</span>
-
-                            <span v-if="animal.data.last_feeding && !animal.data.death_date">
-                                <br />
-                                <i class="material-icons tiny">local_dining</i>
-                                {{ $t(
-                                    'units.' + $getMatchingTimeDiff(animal.data.last_feeding.timestamps.created_diff).unit,
-                                    {val: $getMatchingTimeDiff(animal.data.last_feeding.timestamps.created_diff).val}
-                                )}}
-                                {{ animal.data.last_feeding.name }}
-                            </span>
-
-                            <span v-if="animal.data.last_weighing && !animal.data.death_date">
-                                <br />
-                                <i class="material-icons tiny">file_download</i>
-                                {{ $t(
-                                    'units.' + $getMatchingTimeDiff(animal.data.last_weighing.timestamps.created_diff).unit,
-                                    {val: $getMatchingTimeDiff(animal.data.last_weighing.timestamps.created_diff).val}
-                                )}}
-                                {{ animal.data.last_weighing.value }}{{ animal.data.last_weighing.name }}
-                                <span v-if="animal.data.last_weighing.trend && animal.data.last_weighing.trend > 0" class="green-text tooltipped"
-                                      data-delay="50" data-html="true"
-                                      :data-tooltip="'<div style=\'max-width: 300px\'>' + $t('tooltips.animal_weighing.trend') + '</div>'">
-                                    (+ {{ animal.data.last_weighing.trend }}%)
-                                </span>
-                                <span v-if="animal.data.last_weighing.trend && animal.data.last_weighing.trend < 0" class="red-text tooltipped"
-                                      data-delay="50" data-html="true"
-                                      :data-tooltip="'<div style=\'max-width: 300px\'>' + $t('tooltips.animal_weighing.trend') + '</div>'">
-                                    ({{ animal.data.last_weighing.trend }}%)
-                                </span>
-                                <span v-if="animal.data.last_weighing.trend && animal.data.last_weighing.trend == 0" class="tooltipped"
-                                      data-delay="50" data-html="true"
-                                      :data-tooltip="'<div style=\'max-width: 300px\'>' + $t('tooltips.animal_weighing.trend') + '</div>'">
-                                    (+/- 0%)
-                                </span>
-                            </span>
-                        </div>
-                    </div>
-
-                    <div class="card-reveal" v-if="!animal.data.death_date">
-                        <div>
-                            <strong>{{ animal.data.display_name }}</strong>
-                            <i class="material-icons right card-title card-title-small">close</i>
+                            <div class="card-title">
+                                <span><a :href="'/animals/' + animal.data.id">{{ animal.data.display_name }}</a></span>
+                                <loading-indicator :size="20" v-show="animal.data.loading_data"> </loading-indicator>
+                                <a href="#!"><i class="material-icons right activator" v-if="!animal.data.death_date">more_vert</i></a>
+                            </div>
                         </div>
 
-                        <p v-if="terrarium = terraria.filter(t => t.id === animal.data.terrarium_id)[0]">
-                            <i class="material-icons">video_label</i>
-                            <a :href="'/terraria/' + terrarium.data.id">{{ terrarium.data.display_name }}</a>
-                        </p>
+                        <div class="card-content">
+                            <div>
+                                <span v-show="animal.data.latin_name">{{ animal.data.latin_name }},</span>
+                                <span v-show="animal.data.common_name && !animal.data.latin_name">{{ animal.data.common_name }},</span>
+                                <span v-show="animal.data.birth_date || animal.data.death_date">{{ animal.data.age_value }} {{ $tc("units." + animal.data.age_unit, animal.data.age_value) }}</span>
 
-                        <p>
-                            <i class="material-icons">play_arrow</i>
-                            <a href="#!" v-bind:href="'#modal_just_fed_' + animal.data.id" v-bind:onclick="'$(\'#modal_just_fed_' + animal.data.id + '\').modal(); $(\'#modal_just_fed_' + animal.data.id + ' select\').material_select(); $(\'#modal_just_fed_' + animal.data.id + '\').modal(\'open\');'">{{ $t("labels.just_fed") }}</a>
-                        </p>
+                                <span v-if="animal.data.last_feeding && !animal.data.death_date">
+                                    <br />
+                                    <i class="material-icons tiny">local_dining</i>
+                                    {{ $t(
+                                        'units.' + $getMatchingTimeDiff(animal.data.last_feeding.timestamps.created_diff).unit,
+                                        {val: $getMatchingTimeDiff(animal.data.last_feeding.timestamps.created_diff).val}
+                                    )}}
+                                    {{ animal.data.last_feeding.name }}
+                                </span>
 
-                        <p>
-                            <i class="material-icons">play_arrow</i>
-                            <a href="#!" v-bind:href="'#modal_add_weight_' + animal.data.id" v-bind:onclick="'$(\'#modal_add_weight_' + animal.data.id + '\').modal(); $(\'#modal_add_weight_' + animal.data.id + ' select\').material_select(); $(\'#modal_add_weight_' + animal.data.id + '\').modal(\'open\');'">{{ $t("labels.add_weight") }}</a>
-                        </p>
+                                <span v-if="animal.data.last_weighing && !animal.data.death_date">
+                                    <br />
+                                    <i class="material-icons tiny">file_download</i>
+                                    {{ $t(
+                                        'units.' + $getMatchingTimeDiff(animal.data.last_weighing.timestamps.created_diff).unit,
+                                        {val: $getMatchingTimeDiff(animal.data.last_weighing.timestamps.created_diff).val}
+                                    )}}
+                                    {{ animal.data.last_weighing.value }}{{ animal.data.last_weighing.name }}
+                                    <span v-if="animal.data.last_weighing.trend && animal.data.last_weighing.trend > 0" class="green-text tooltipped"
+                                          data-delay="50" data-html="true"
+                                          :data-tooltip="'<div style=\'max-width: 300px\'>' + $t('tooltips.animal_weighing.trend') + '</div>'">
+                                        (+ {{ animal.data.last_weighing.trend }}%)
+                                    </span>
+                                    <span v-if="animal.data.last_weighing.trend && animal.data.last_weighing.trend < 0" class="red-text tooltipped"
+                                          data-delay="50" data-html="true"
+                                          :data-tooltip="'<div style=\'max-width: 300px\'>' + $t('tooltips.animal_weighing.trend') + '</div>'">
+                                        ({{ animal.data.last_weighing.trend }}%)
+                                    </span>
+                                    <span v-if="animal.data.last_weighing.trend && animal.data.last_weighing.trend == 0" class="tooltipped"
+                                          data-delay="50" data-html="true"
+                                          :data-tooltip="'<div style=\'max-width: 300px\'>' + $t('tooltips.animal_weighing.trend') + '</div>'">
+                                        (+/- 0%)
+                                    </span>
+                                </span>
+                            </div>
+                        </div>
+
+                        <div class="card-reveal" v-if="!animal.data.death_date">
+                            <div>
+                                <strong>{{ animal.data.display_name }}</strong>
+                                <i class="material-icons right card-title card-title-small">close</i>
+                            </div>
+
+                            <p v-if="terrarium = terraria.filter(t => t.id === animal.data.terrarium_id)[0]">
+                                <i class="material-icons">video_label</i>
+                                <a :href="'/terraria/' + terrarium.data.id">{{ terrarium.data.display_name }}</a>
+                            </p>
+
+                            <p>
+                                <i class="material-icons">play_arrow</i>
+                                <a href="#!" v-bind:href="'#modal_just_fed_' + animal.data.id" v-bind:onclick="'$(\'#modal_just_fed_' + animal.data.id + '\').modal(); $(\'#modal_just_fed_' + animal.data.id + ' select\').material_select(); $(\'#modal_just_fed_' + animal.data.id + '\').modal(\'open\');'">{{ $t("labels.just_fed") }}</a>
+                            </p>
+
+                            <p>
+                                <i class="material-icons">play_arrow</i>
+                                <a href="#!" v-bind:href="'#modal_add_weight_' + animal.data.id" v-bind:onclick="'$(\'#modal_add_weight_' + animal.data.id + '\').modal(); $(\'#modal_add_weight_' + animal.data.id + ' select\').material_select(); $(\'#modal_add_weight_' + animal.data.id + '\').modal(\'open\');'">{{ $t("labels.add_weight") }}</a>
+                            </p>
+                        </div>
                     </div>
-                </div>
+                </template>
             </div>
         </div>
     </div>
